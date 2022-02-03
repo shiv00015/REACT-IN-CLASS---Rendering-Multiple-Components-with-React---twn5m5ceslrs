63089c024642428854b05142c652a1c4bd3e2816
Shivprasad <vicbirajdar15gmail.com>
2022-02-03 19:01:55 +0530

    add all

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
+import { render } from "enzyme";
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <div id="main">
+      <h1 data-ns-test="project-name">Lyrics APP</h1>
+      <p data-ns-test="project-description">In this Lyrics APP we can get lyrics of a song by entering either song name or artist name of respective song, when we enter song name or artist name we has to press a button after pressing it, it lists out all songs of an artist in list formate each list has a get lyrics button , by pressing it will display a lyrics of respective song</p>
+    </div>
+ )
 }
 
 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from "./components/App";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />,document.getElementById("root"));
